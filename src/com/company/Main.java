1130b692a2516fc1c991196e07a653d38e6b5b43
John Mario <[john1646@hotmail.com]>
2021-09-03 21:04:11 -0500

    segundo comit arreglo

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,17 +8,22 @@ public class Main {
 	// write your code here
 
         Scanner objentrada = new Scanner(System.in);
-        int [] numeros=new int[4];
+        Persona [] personas=new Persona[4];
 
-        for(int i=0; i < numeros.length; i++){
-            System.out.println("Digita el dato: "+ i);
-            numeros[i] = objentrada.nextInt();
+        for(int i=0; i < personas.length; i++){
+
+            personas[i] = new Persona();
+
+            System.out.println("Digita el nombre de la persona: "+ i);
+            personas[i].setNombre(objentrada.next());
         }
 
         System.out.println("########################################################");
 
-        for(int i=0; i < numeros.length; i++){
-            System.out.println("dato "+ numeros [i]);
+        for(int i=0; i < personas.length; i++){
+
+            System.out.println(personas[i].getNombre());
+
 
         }
 
